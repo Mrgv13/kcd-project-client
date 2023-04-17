1425fccc0608c97f1f7df8d13028d605054c9559
Максим Доронкин <maksim@MacBook-Pro-Maksim.local>
2023-04-17 15:54:29 +0300

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import AppRouter from './components/AppRouter';
+import AppRouter from './components/AppRouter'
 
-import NavBar from './components/navbar/NavBar';
+import NavBar from './components/navbar/NavBar'
 
-import { BrowserRouter } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function App() {
-  const auth = useSelector((state) => state.auth.auth[0].type);
+  const auth = useSelector((state) => state.auth.auth[0].type)
+
   return (
     <BrowserRouter>
       {auth ? <NavBar /> : <></>}
       <AppRouter />
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
